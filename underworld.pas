3e119aeb7b07aac1f4e60ae299bd82478ed746cd
Alexander Larin <ekzebox@gmail.com>
2016-04-05 14:57:28 +0300

    basic game cycle has been added

--- underworld.pas
+++ underworld.pas
@@ -8,10 +8,37 @@ uses
 	windows,
 	{$ENDIF}
 	math;
- 
+	
+procedure Initialize();
+begin
+	WriteLn('[+]Initizlization');
+end;
+
+procedure Finalize();
+begin
+	WriteLn('[+]Finalization');
+end;
+
+function Fall(): Boolean;
+begin
+	WriteLn('[-]Falling');
+	Fall := True;
+end;
+
+var
+	isFalling: Boolean;
+	depth: Integer;
+	
 begin
 	{$IFDEF WINDOWS}
 	SetConsoleOutputCP(CP_UTF8);
 	{$ENDIF}
 	WriteLn('Привет, Дно.');
+	
+	Initialize();
+	repeat
+		isFalling := Fall();
+		Inc(depth);
+	until (not isFalling or (depth = 10));
+	Finalize();
 end.
