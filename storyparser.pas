ca7a24a9d67b9e26dcd77bd85c55bae6261143d1
Alexander Larin <ekzebox@gmail.com>
2016-05-10 15:14:38 +0300

    Refactoring of parser begins.

--- storyparser.pas
+++ storyparser.pas
@@ -4,41 +4,113 @@ interface
 	uses
 		SysUtils, Types;
 	function ReadToken(var text: TextFile; var token: String): Boolean;
+	function ReadEvent(var text: TextFile; var event: TEvent): Boolean;
+	function ReadEvents(var text: TextFile; var events: TEvents): Boolean;
+	function ReadCommand(var text: TextFile; var command: TCommand): Boolean;
+	function ReadCommands(var text: TextFile; var commands: TCommands): Boolean;
 	function LoadStory(filename: String; var events: TEvents): Boolean;
 
 implementation
+	function ReadCommand(var text: TextFile; var command: TCommand): Boolean;
+	var
+		token: String;
+	begin
+	begin
+		token := '';
+		if not ReadToken(text, command.name) then
+			Exit(False);
+		while ReadToken(text, token) do
+		begin
+			if token = 'text' then
+				ReadToken(text, event.text);
+			if token = 'cmd' then
+				ReadToken(text, event.cmd);
+			if token = 'toEvent' then
+				ReadToken(text, event.toEvent);
+			if token = 'end' then
+			begin
+				ReadToken(text, token);
+				if token = 'command' then
+					Exit(True);
+			end
+		end;
+	end;
+	
+	function ReadCommands(var text: TextFile; var commands: TCommands): Boolean;
+	begin
+		ReadToken(text, StrToInt(CommandCount));
+		for i:= 0 to CommandCount - 1 do
+			ReadCommand(text, event.commands[CommandCount]);
+	end;
+	
+	function ReadEvent(var text: TextFile; var event: TEvent): Boolean;
+	var
+		token: String;
+		CommandCount: Integer;
+	begin
+		token := '';
+		if ReadToken(text, event.name) then
+		begin
+			ReadToken(text, token);
+			if token = 'text' then
+				ReadToken(text, event.text);
+			if token = 'commands' then
+			begin
+				ReadCommands(text, event.commands);
+				
+			end;
+			if token = 'cmd' then
+				ReadToken(text, event.t);
+			if token = 'text' then
+				ReadToken(text, event.text);
+			if (s = 'cmd') then
+			begin
+				ReadToken(text, s);
+				events[EventCount].commands[CommandCount].cmd := s;
+				continue;
+			end;
+			
+			if (s = 'toEvent') then
+			begin
+				ReadToken(text, s);
+				events[EventCount].commands[CommandCount].toEvent := s;
+				continue;
+			end;
+		end;
+	end;
+	
 	function ReadToken(var text: TextFile; var token: String): Boolean;
 	var
+		isStarted, isSpecial: Boolean;
 		ch: Char;
-		isStarted: Boolean;
 	begin
 		isStarted := False;
 		token := '';
 		while not EOF(text) do
 		begin
 			Read(text, ch);
-			if ch in ['A'..'Z', 'a'..'z', '0'..'9'] then
-				isStarted := True
-			else
-				if isStarted then
-					break;
-			if isStarted then
-				token := token + ch;
-			
-			if ch = '''' then
+			if ch in ['A'..'Z', 'a'..'z', '0'..'9', ''''] then
 			begin
-				isStarted:= True;
-				while not EOF(text) do
+				if ch = '''' then
 				begin
-					Read(text, ch);
-					if (not (ch = '''')) then
-						token := token + ch
+					isSpecial := True;
+					if not isStarted then
+						isStarted := True
 					else
 						break;
-				end;
+				end
+				else
+					isStarted := True;
+			end
+			else
+			begin
+				if not isSpecial then
+					break;
 			end;
+			
+			if isStarted then
+				token := token + ch;		
 		end;
-		WriteLn('Token: ', token);
 		ReadToken := not EOF(text);
 	end;
 	
