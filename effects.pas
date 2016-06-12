325b24f3a32e70ddcd5f67ff8bd5688e75bdd087
Kretinin Ilya <dantin@bk.ru>
2016-06-12 16:19:28 +0300

    Effects has been corrected.

--- effects.pas
+++ effects.pas
@@ -2,7 +2,10 @@ unit Effects;
 
 interface
 	uses
-		types;
+		types,
+		sysutils,
+		outputcolor;
+		
 	function Affect(var hero: THero; var antiHero: THero; effect: TEffect): Boolean;
 	function Affect(var hero: THero; var antiHero: THero; effects: TEffects): Boolean;
 	
@@ -34,6 +37,7 @@ implementation
 	function Damage(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.health := hero.health - effect.value;
+		effect.text := 'Ты словил леща на ' + IntToStr(effect.value) + '0%';
 		if hero.health < 0 then
 			hero.health := 0;
 		if hero.health = 0 then
@@ -44,6 +48,7 @@ implementation
 	function DamageAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.health := antiHero.health - effect.value;
+		effect.text := 'Ты словил леща на ' + IntToStr(effect.value) + '0%';
 		if antiHero.health < 0 then
 			antiHero.health := 0;
 		if antiHero.health = 0 then
@@ -54,46 +59,50 @@ implementation
 	function Heal(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.health := hero.health + effect.value;
+		effect.text := 'Хильнулся на ' + IntToStr(effect.value) + '0%';
 		if hero.health > 10 then
 			hero.health := 10;
 		if hero.health = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function HealAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.health := antiHero.health + effect.value;
-		if antiHero.health > 10 then
+		effect.text := 'Хильнулся на ' + IntToStr(effect.value) + '0%';
 			antiHero.health := 10;
 		if antiHero.health = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 
 	function Boost(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.energy := hero.energy + effect.value;
+		effect.text := 'Энергетик помог на ' + IntToStr(effect.value) + '0%';
 		if hero.energy > 10 then
 			hero.energy := 10;
 		if hero.energy = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function BoostAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.energy := antiHero.energy + effect.value;
+		effect.text := 'Энергетик помог на ' + IntToStr(effect.value) + '0%';
 		if antiHero.energy > 10 then
 			antiHero.energy := 10;
 		if antiHero.energy = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function Decrease(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.energy := hero.energy - effect.value;
+		effect.text := 'Приблизился ко сну на ' + IntToStr(effect.value) + '0%';
 		if hero.energy < 0 then
 			hero.energy := 0;
 		if hero.energy = 0 then
@@ -104,6 +113,7 @@ implementation
 	function DecreaseAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.energy := antiHero.energy - effect.value;
+		effect.text := 'Приблизился ко сну на ' + IntToStr(effect.value) + '0%';
 		if antiHero.energy < 0 then
 			antiHero.energy := 0;
 		if antiHero.energy = 0 then
@@ -114,321 +124,353 @@ implementation
 	function TakeToDrink(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.alchohol := hero.alchohol + effect.value;
+		effect.text := 'Напился на ' + IntToStr(effect.value) + '0%';
 		if hero.alchohol > 10 then
 			hero.alchohol := 10;
 		if hero.alchohol = 10 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function TakeToDrinkAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.alchohol := antiHero.alchohol + effect.value;
+		effect.text := 'Напился на ' + IntToStr(effect.value) + '0%';
 		if antiHero.alchohol > 10 then
 			antiHero.alchohol := 10;
 		if antiHero.alchohol = 10 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function Sober(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.alchohol := hero.alchohol - effect.value;
+		effect.text := 'Отдалился от "хорошего настроения" на' + IntToStr(effect.value) + '0%';
 		if hero.alchohol < 0 then
 			hero.alchohol := 0;
 		if hero.alchohol = 0 then 
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function SoberAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.alchohol := antiHero.alchohol - effect.value;
+		effect.text := 'Отдалился от "хорошего настроения" на' + IntToStr(effect.value) + '0%';
 		if antiHero.alchohol < 0 then
 			antiHero.alchohol := 0;
 		if antiHero.alchohol = 0 then 
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function PumpUp(var hero: THero; effect: TEffect): Boolean; 
 	begin
 		hero.strength := hero.strength + effect.value;
+		effect.text := 'Твой гребешок вырос на ' + IntToStr(effect.value) + '0%';
 		if hero.strength > 10 then
 			hero.strength := 10;
 		if hero.strength = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function PumpUpAnti(var antiHero: THero; effect: TEffect): Boolean; 
 	begin
 		antiHero.strength := antiHero.strength + effect.value;
+		effect.text := 'Твой гребешок вырос на ' + IntToStr(effect.value) + '0%';
 		if antiHero.strength > 10 then
 			antiHero.strength := 10;
 		if antiHero.strength = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function PumpDown(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.strength := hero.strength - effect.value;
+		effect.text := 'Отдалился от турничков на ' + IntToStr(effect.value) + '0%';
 		if hero.strength < 0 then
 			hero.strength := 0;
 		if hero.strength = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function PumpDownAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.strength := antiHero.strength - effect.value;
+		effect.text := 'Отдалился от турничков на ' + IntToStr(effect.value) + '0%';
 		if antiHero.strength < 0 then
 			antiHero.strength := 0;
 		if antiHero.strength = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function ReduceIntelligence(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.intelligence := hero.strength - effect.value;
+		effect.text := 'Шутки Левина стали смешнее на ' + IntToStr(effect.value) + '0%';
 		if hero.intelligence < 0 then
 			hero.intelligence := 0;
 		if hero.intelligence = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function ReduceIntelligenceAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.intelligence := antiHero.strength - effect.value;
+		effect.text := 'Шутки Левина стали смешнее на ' + IntToStr(effect.value) + '0%';
 		if antiHero.intelligence < 0 then
 			antiHero.intelligence := 0;
 		if antiHero.intelligence = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function IncreaseIntelligence(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.intelligence := hero.strength + effect.value;
+		effect.text := 'Обыдлел на ' + IntToStr(effect.value) + '0%';
 		if hero.intelligence > 10 then
 			hero.intelligence := 10;
 		if hero.intelligence = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function IncreaseIntelligenceAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.intelligence := antiHero.strength + effect.value;
+		effect.text := 'Обыдлел на ' + IntToStr(effect.value) + '0%';
 		if antiHero.intelligence > 10 then
 			antiHero.intelligence := 10;
 		if antiHero.intelligence = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	
 	function Luckly(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.fortune := hero.fortune + effect.value;
+		effect.text := 'На улице стало меньше чёрных кошек на ' + IntToStr(effect.value) + '0%';
 		if hero.fortune > 10 then
 			hero.fortune := 10;
 		if hero.fortune = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function LucklyAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.fortune := antiHero.fortune + effect.value;
+		effect.text := 'На улице стало меньше чёрных кошек на ' + IntToStr(effect.value) + '0%';
 		if antiHero.fortune > 10 then
 			antiHero.fortune := 10;
 		if antiHero.fortune = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function Luckless(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.fortune := hero.fortune - effect.value;
+		effect.text := 'На улице стало больше баб с пустыми вёдрами на ' + IntToStr(effect.value) + '0%';
 		if hero.fortune < 0 then
 			hero.fortune := 0;
 		if hero.fortune = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function LucklessAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.fortune := antiHero.fortune - effect.value;
+		effect.text := 'На улице стало больше баб с пустыми вёдрами на ' + IntToStr(effect.value) + '0%';
 		if antiHero.fortune < 0 then
 			antiHero.fortune := 0;
 		if antiHero.fortune = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function Lonely(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.love := hero.love - effect.value;
+		effect.text := 'Одна рука стала более накаченной на ' + IntToStr(effect.value) + '0%';
 		if hero.love < 0 then
 			hero.love := 0;
 		if hero.love = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function LonelyAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.love := antiHero.love - effect.value;
+		effect.text := 'Одна рука стала более накаченной на ' + IntToStr(effect.value) + '0%';
 		if antiHero.love < 0 then
 			antiHero.love := 0;
 		if antiHero.love = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function InLove(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.love := hero.love + effect.value;
+		effect.text := 'Подкат на гелике стал ближе на ' + IntToStr(effect.value) + '0%';
 		if hero.love < 0 then
 			hero.love := 0;
 		if hero.love = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function InLoveAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.love := antiHero.love + effect.value;
+		effect.text := 'Подкат на гелике стал ближе на ' + IntToStr(effect.value) + '0%';
 		if antiHero.love < 0 then
 			antiHero.love := 0;
 		if antiHero.love = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
   function Happiness(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.happy := hero.happy + effect.value;
+		effect.text := 'Стал радостнее на ' + IntToStr(effect.value) + '0%';
 		if hero.happy < 0 then
 			hero.happy := 0;
 		if hero.happy = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function HappinessAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.happy := antiHero.happy + effect.value;
+		effect.text := 'Стал радостнее на ' + IntToStr(effect.value) + '0%';
 		if antiHero.happy < 0 then
 			antiHero.happy := 0;
 		if antiHero.happy = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function Misfortune(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.happy := hero.happy - effect.value;
+		effect.text := 'Хочешь вернуть 2007-ой на ' + IntToStr(effect.value) + '0%';
 		if hero.happy < 0 then
 			hero.happy := 0;
 		if hero.happy = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function MisfortuneAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.happy := antiHero.happy - effect.value;
+		effect.text := 'Хочешь вернуть 2007-ой на ' + IntToStr(effect.value) + '0%';
 		if antiHero.happy < 0 then
 			antiHero.happy := 0;
 		if antiHero.happy = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function UpReputationInGroup(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.reputationInGroup := hero.reputationInGroup + effect.value;
+		effect.text := 'Проявляющих своё увОжение на ' + IntToStr(effect.value) + '0%';
 		if hero.reputationInGroup > 10 then
 			hero.reputationInGroup := 10;
 		if hero.reputationInGroup = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function UpReputationInGroupAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.reputationInGroup := antiHero.reputationInGroup + effect.value;
+		effect.text := 'Проявляющих своё увОжение на ' + IntToStr(effect.value) + '0%';
 		if antiHero.reputationInGroup > 10 then
 			antiHero.reputationInGroup := 10;
 		if antiHero.reputationInGroup = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function DownReputationInGroup(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.reputationInGroup := hero.reputationInGroup - effect.value;
+		effect.text := 'Ржущих над тобой стало больше на ' + IntToStr(effect.value) + '0%';
 		if hero.reputationInGroup < 0 then
 			hero.reputationInGroup := 0;
 		if hero.reputationInGroup = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function DownReputationInGroupAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.reputationInGroup := antiHero.reputationInGroup - effect.value;
+		effect.text := 'Ржущих над тобой стало больше на ' + IntToStr(effect.value) + '0%';
 		if antiHero.reputationInGroup < 0 then
 			antiHero.reputationInGroup := 0;
 		if antiHero.reputationInGroup = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function UpReputationInUniversity(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.reputationInUnderworld := hero.reputationInUnderworld + effect.value;
+		effect.text := 'Грязев запомнил тебя на ' + IntToStr(effect.value) + '0%';
 		if hero.reputationInUnderworld > 10 then
 			hero.reputationInUnderworld := 10;
 		if hero.reputationInUnderworld = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function UpReputationInUniversityAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.reputationInUnderworld := antiHero.reputationInUnderworld + effect.value;
+		effect.text := 'Грязев запомнил тебя на ' + IntToStr(effect.value) + '0%';
 		if antiHero.reputationInUnderworld > 10 then
 			antiHero.reputationInUnderworld := 10;
 		if antiHero.reputationInUnderworld = 10 then
 			Exit(True);
-		Exit(False);
+		Exit(True);
 	end;
 	
 	function DownReputationInUniversity(var hero: THero; effect: TEffect): Boolean;
 	begin
 		hero.reputationInUnderworld := hero.reputationInUnderworld - effect.value;
+		effect.text := 'Стало обычнее на ' + IntToStr(effect.value) + '0%';
 		if hero.reputationInUnderworld < 0 then
 			hero.reputationInUnderworld := 0;
 		if hero.reputationInUnderworld = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
 	function DownReputationInUniversityAnti(var antiHero: THero; effect: TEffect): Boolean;
 	begin
 		antiHero.reputationInUnderworld := antiHero.reputationInUnderworld - effect.value;
+		effect.text := 'Стало обычнее на ' + IntToStr(effect.value) + '0%';
 		if antiHero.reputationInUnderworld < 0 then
 			antiHero.reputationInUnderworld := 0;
 		if antiHero.reputationInUnderworld = 0 then
-			Exit(False);
+			Exit(True);
 		Exit(True);
 	end;
 	
@@ -563,8 +605,11 @@ implementation
 	begin
 		// TODO: console output
 		for I := 0 to Length(effects) - 1 do
+		begin
 			if not Affect(hero, antiHero, effects[I]) then
 				Exit(False);
+			ColorWrite(effects[I].text, 'LightGreen', 1);
+		end;
 		Exit(True);
 	end;
 	
