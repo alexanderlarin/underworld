0346556df5efb9ec6b68629c67da1fcc4bdf4ccc
Белоусов Владислав <belousov0711@mail.ru>
2016-06-19 21:59:28 +0800

    Story has been completed. Some changes in game-files has been added (or deleted). After exams work will continue.

--- conditions.pas
+++ conditions.pas
@@ -6,33 +6,59 @@ interface
 
 	function CheckCondition(hero: THero; antiHero: THero; condition: TCondition): Boolean;
 	function CheckConditions(hero: THero; antiHero: THero; conditions: TConditions): Boolean;
+	function ConditionReputationInGroupMore(hero: THero; value: Integer): Boolean;
+	function ConditionReputationInGroupLess(hero: THero; value: Integer): Boolean;
+	function ConditionReputationInUniversityMore(hero: THero; value: Integer): Boolean;
+	function ConditionReputationInUniversityLess(hero: THero; value: Integer): Boolean;
+	function ConditionVapeMore(hero: THero; value: Integer): Boolean;
+	function ConditionVapeLess(hero: THero; value: Integer): Boolean;
+	function ConditionHappyMore(hero: THero; value: Integer): Boolean;
+	function ConditionHappyLess(hero: THero; value: Integer): Boolean;
 	function ConditionTuningMore(hero: THero; value: Integer): Boolean;
+	function ConditionInLoveMore(hero: THero; value: Integer): Boolean;
 	function ConditionInLoveLess(hero: THero; value: Integer): Boolean;
+	function ConditionFortuneMore(hero: THero; value: Integer): Boolean;
+	function ConditionFortuneLess(hero: THero; value: Integer): Boolean;
+	function ConditionIntelligenceMore(hero: THero; value: Integer): Boolean;
+	function ConditionIntelligenceLess(hero: THero; value: Integer): Boolean;
 	function ConditionStrengthMore(hero: THero; value: Integer): Boolean;
 	function ConditionStrengthLess(hero: THero; value: Integer): Boolean;
-	function ConditionStrengthEqual(hero: THero; value: Integer): Boolean;
 	function ConditionStrengthAntiMore(hero: THero; antiHero: THero): Boolean;
 	function ConditionStrengthAntiLess(hero: THero; antiHero: THero): Boolean;
 	function ConditionAlchoholMore(hero: THero; value: Integer): Boolean;
+	function ConditionAlchoholLess(hero: THero; value: Integer): Boolean;
 	function ConditionHealthMore(hero: THero; value: Integer): Boolean;
 	function ConditionHealthLess(hero: THero; value: Integer): Boolean;
-	function ConditionHealthEqual(hero: THero; value: Integer): Boolean;
+	function ConditionHealthAntiLess(antiHero: THero; value: Integer): Boolean;
 
 implementation
 	function CheckCondition(hero: THero; antiHero: THero; condition: TCondition): Boolean;
 	begin
 		case condition.name of
+		'ReputationInGroupMore': Exit(ConditionReputationInGroupMore(hero, condition.value));
+		'ReputationInGroupLess': Exit(ConditionReputationInGroupLess(hero, condition.value));
+		'ReputationInUniversityMore': Exit(ConditionReputationInUniversityMore(hero, condition.value));
+		'ReputationInUniversityLess': Exit(ConditionReputationInUniversityLess(hero, condition.value));
+		'VapeMore': Exit(ConditionVapeMore(hero, condition.value));
+		'VapeLess': Exit(ConditionVapeLess(hero, condition.value));
+		'HappyMore': Exit(ConditionHappyMore(hero, condition.value));
+		'HappyLess': Exit(ConditionHappyLess(hero, condition.value));
 		'TuningMore': Exit(ConditionTuningMore(hero, condition.value));
+		'InLoveMore': Exit(ConditionInLoveMore(hero, condition.value));
 		'InLoveLess': Exit(ConditionInLoveLess(hero, condition.value));
+		'IntelligenceMore': Exit(ConditionIntelligenceMore(hero, condition.value));
+		'IntelligenceLess': Exit(ConditionIntelligenceLess(hero, condition.value));
+		'FortuneMore':Exit(ConditionFortuneMore(hero, condition.value));
+		'FortuneLess':Exit(ConditionFortuneLess(hero, condition.value));
 		'StrengthMore': Exit(ConditionStrengthMore(hero, condition.value));
 		'StrengthLess': Exit(ConditionStrengthLess(hero, condition.value));
-		'StrengthEqual': Exit(ConditionStrengthEqual(hero, condition.value));
 		'StrengthAntiMore': Exit(ConditionStrengthAntiMore(hero, antiHero));
 		'StrengthAntiLess': Exit(ConditionStrengthAntiLess(hero, antiHero));
 		'AlchoholMore': Exit(ConditionAlchoholMore(hero, condition.value));
+		'AlchoholLess': Exit(ConditionAlchoholLess(hero, condition.value));
 		'HealthMore': Exit(ConditionHealthMore(hero, condition.value));
 		'HealthLess': Exit(ConditionHealthLess(hero, condition.value));
-		'HealthEqual': Exit(ConditionHealthEqual(hero, condition.value));
+		'HealthAntiLess': Exit( ConditionHealthAntiLess(antiHero, condition.value));
 		end;
 		Exit(True);
 	end;
@@ -49,6 +75,46 @@ implementation
 		Exit(True);
 	end;
 	
+	function ConditionReputationInGroupMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.ReputationInGroup >= value);			
+	end;
+	
+		function ConditionReputationInGroupLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.ReputationInGroup < value);			
+	end;
+	
+	function ConditionReputationInUniversityMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.ReputationInUnderworld >= value);			
+	end;
+	
+	function ConditionReputationInUniversityLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.ReputationInUnderworld < value);			
+	end;
+	
+	function ConditionVapeMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.vape >= value);			
+	end;
+	
+	function ConditionVapeLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.vape < value);			
+	end;
+	
+	function ConditionHappyMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.happy >= value);			
+	end;
+	
+	function ConditionHappyLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.happy < value);			
+	end;
+	
 	function ConditionTuningMore(hero: THero; value: Integer): Boolean;
 	begin
 		Exit(hero.tune > value);			
@@ -59,9 +125,34 @@ implementation
 		Exit(hero.love < value);			
 	end;
 	
+	function ConditionInLoveMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.love >= value);			
+	end;
+	
+	function ConditionFortuneMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.fortune >= value);			
+	end;
+	
+	function ConditionFortuneLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.fortune < value);			
+	end;
+	
+	function ConditionIntelligenceMore(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.intelligence >= value);			
+	end;
+	
+	function ConditionIntelligenceLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.intelligence < value);			
+	end;
+	
 	function ConditionStrengthMore(hero: THero; value: Integer): Boolean;
 	begin
-		Exit(hero.strength > value);			
+		Exit(hero.strength >= value);			
 	end;
 	
 	function ConditionStrengthLess(hero: THero; value: Integer): Boolean;
@@ -69,10 +160,6 @@ implementation
 		Exit(hero.strength < value);
 	end;
 	
-	function ConditionStrengthEqual(hero: THero; value: Integer): Boolean;
-	begin
-		Exit(hero.strength = value);
-	end;
 	
 	function ConditionStrengthAntiMore(hero: THero; antiHero: THero): Boolean;
 	begin
@@ -83,10 +170,15 @@ implementation
 	begin
 		Exit(hero.strength < antiHero.strength);
 	end;
-
+	
 	function ConditionAlchoholMore(hero: THero; value: Integer): Boolean;
 	begin
-		Exit(hero.alchohol > value);			
+		Exit(hero.alchohol >= value);			
+	end;
+	
+	function ConditionAlchoholLess(hero: THero; value: Integer): Boolean;
+	begin
+		Exit(hero.alchohol < value);			
 	end;
 	
 	function ConditionHealthMore(hero: THero; value: Integer): Boolean;
@@ -96,27 +188,12 @@ implementation
 	
 	function ConditionHealthLess(hero: THero; value: Integer): Boolean;
 	begin
-		Exit(hero.health < value);
+		Exit(hero.health <= value);
 	end;
 	
-	function ConditionHealthEqual(hero: THero; value: Integer): Boolean;
+	function ConditionHealthAntiLess(antiHero: THero; value: Integer): Boolean;
 	begin
-		Exit(hero.health = value);
+		Exit(antiHero.health <= value);
 	end;
-	
-	function ConditionHealthAntiMore(hero: THero; antiHero: THero): Boolean;
-	begin
-		Exit(hero.health > antiHero.health);
-	end;
-	
-	function ConditionHealthAntiLess(hero: THero; antiHero: THero): Boolean;
-	begin
-		Exit(hero.health < antiHero.health);
-	end;
-	
-	function ConditionHealthAntiEqual(hero: THero; antiHero: THero): Boolean;
-	begin
-		Exit(hero.health = antiHero.health);
-	end;
-	
+
 end.
